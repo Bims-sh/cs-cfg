f4027b9cafff4a42a5919a4a89a913ecab9203ad
Bims <dasischbims@gmail.com>
2024-10-09 19:56:12 +0200

    add screen res scripts

---
+++ scripts/16by9.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+
+ChangeScreenResolution.exe /w=2560 /h=1440 /d=1
+
+endlocal
---
+++ scripts/4by3.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+
+ChangeScreenResolution.exe /w=1920 /h=1440 /d=1
+
+endlocal
